0b9ce3450457a5da09328a03fb3e1aea42900fc2
vtsuellen <vtsuellen@hotmail.com>
2024-01-30 11:01:59 -0300

    feat: testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message('Harry', '5')
+    
+    with pytest.raises(TypeError):
+        encrypt_message(543, 5)
+
+    assert encrypt_message('Harry', 5) == 'yrr_aH'
+    assert encrypt_message('Harry', 0) == 'H_arry'
