dc4c7c9d63e433415be1fc23bfc195aea3768b63
vtsuellen <vtsuellen@hotmail.com>
2024-01-30 11:15:34 -0300

    fix: testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,9 +4,9 @@ import pytest
 
 def test_encrypt_message():
     with pytest.raises(TypeError):
-        encrypt_message('Harry', '5')
+        encrypt_message('teste', '5')
     with pytest.raises(TypeError):
         encrypt_message(543, 5)
 
-    assert encrypt_message('Harry', 5) == 'yrr_aH'
-    assert encrypt_message('Harry', 0) == 'H_arry'
+    assert encrypt_message('teste', 5) == 't_este'
+    assert encrypt_message('teste', 0) == 'etse_t'
