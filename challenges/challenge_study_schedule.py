6507c46be91a302721a9984ff0e24262bee446a5
vtsuellen <vtsuellen@hotmail.com>
2024-01-30 10:51:18 -0300

    feat: challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    students_count = 0 
+
+    try: 
+        for student in permanence_period:
+            if student[0] <= target_time <= student[1]:
+                students_count += 1
+        return students_count
+    
+    except TypeError:
+        return None
