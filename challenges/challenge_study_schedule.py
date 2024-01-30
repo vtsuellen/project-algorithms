00ed1a27493a19d85896d8ba63accf84833a98aa
vtsuellen <vtsuellen@hotmail.com>
2024-01-30 10:54:00 -0300

    fix: lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    students_count = 0 
+    students_count = 0
 
-    try: 
+    try:
         for student in permanence_period:
             if student[0] <= target_time <= student[1]:
                 students_count += 1
         return students_count
-    
     except TypeError:
         return None
