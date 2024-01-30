f72a6f57bb722fe71be276fea18d128b1e093f9a
vtsuellen <vtsuellen@hotmail.com>
2024-01-30 11:05:24 -0300

    fix: lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,10 +1,10 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
+
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message('Harry', '5')
-    
     with pytest.raises(TypeError):
         encrypt_message(543, 5)
 
